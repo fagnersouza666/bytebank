c8e9023e3826b068e4a6d1f2d09fed8b12d2bdbc
Fagner Souza <fagnersouza@gmail.com>
2021-11-27 11:27:10 -0300

    init

---
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-nova-transferencia',
+    templateUrl: './nova-transferencia.components.html',
+    styleUrls: ['./nova-transferecia.component.scss']
+})
+export class NovaTransferenciaComponent{
+
+}
